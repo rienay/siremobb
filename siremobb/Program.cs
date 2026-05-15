6939a3ffa4cc07f20697e0999f046a03de694caf
HanaMirai277 <raihanah12772@gmail.com>
2026-05-15 13:16:05 +0700

    dari hana

--- siremobb/Program.cs
+++ siremobb/Program.cs
@@ -17,6 +17,7 @@ namespace siremobb
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+// ini dari hana
         }
     }
 }
